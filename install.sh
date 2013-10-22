da661793ccf384983370b30abc3f9eb991229220
user.name <user@example.com>
2013-10-22 22:52:18 +0200

    added bin + install script

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+function abs_path () {
+	pushd $(dirname $1) > /dev/null
+	echo $(pwd)
+	popd > /dev/null
+}
+
+DOT_FILES=$HOME/.dot-files
+
+ln -s $(abs_path $0) $DOT_FILES
+
+ln -s $DOT_FILES/zsh/zshrc $HOME/.zshrc
+ln -s $DOT_FILES/git/gitconfig .gitconfig
+ln -s $DOT_FILES/hg/hgrc .hgrc
+ln -s $DOT_FILES/bin $HOME/bin
+
