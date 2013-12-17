f4c70fa1fa9475eb5a97fc73fefccf1eed1a3d97
kevin.urban <kevin.urban@stud.unibas.ch>
2013-12-17 01:07:15 +0100

    macvim: use homebrews --override-system-vim option instead of symlinking it by hand

--- zsh/source/macvim.sh
+++ zsh/source/macvim.sh
@@ -4,14 +4,7 @@
 if ! binary-installed mvim; then
 	echo "installing python + vim from homebrew"
 	brew install python
-	brew install vim
+	brew install macvim --override-system-vim
 fi
-
-VIM_BIN=$USER_BIN/vim
-if [ ! -f $VIM_BIN ]; then
-	echo "symlinking mvim to $USER_BIN"
-	ln -s $(which mvim) $VIM_BIN
-fi
-unset VIM_BIN
 
 export EDITOR="mvim -f"
